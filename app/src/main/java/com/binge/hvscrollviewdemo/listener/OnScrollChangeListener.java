a0dfd9695da9ef846009a65d860bf38c5f1931de
54binge <54binge54@gmail.com>
2015-12-08 18:14:42 +0800

    滚动

---
+++ app/src/main/java/com/binge/hvscrollviewdemo/listener/OnScrollChangeListener.java
@@ -0,0 +1,8 @@
+package com.binge.hvscrollviewdemo.listener;
+
+/**
+ * Created by xzz64 on 2015/12/8.
+ */
+public interface OnScrollChangeListener {
+    void onScrollChange(int scrollX, int scrollY, int oldScrollX, int oldScrollY);
+}
